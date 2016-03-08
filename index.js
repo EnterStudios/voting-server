e3a0ec15f79dce4535ec510dd67305479c85fd9f
Ro Savage <rowan.savage@gmail.com>
2016-03-08 15:29:47 +1300

    Added socket.io emits and ons for recieving and displaying votes

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ import makeStore from './src/store'
 import startServer from './src/server'
 
 export const store = makeStore()
-startServer()
+startServer(store)
+
+// Populate with some fake entries for testing
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+
+// Start the contest
+store.dispatch({type: 'NEXT'})
